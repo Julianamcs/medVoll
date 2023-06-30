a55f53a47e62607071353a9cb13dbc4ad1156904
Juliana Maciel <julyanamcs@gmail.com>
2023-06-30 08:07:50 -0300

    Controller, enum e class
    Controller, enum e class

--- src/main/java/med/voll/api/ApiApplication.java
+++ src/main/java/med/voll/api/ApiApplication.java
@@ -1,4 +1,4 @@
-package med.voll.api.Controller;
+package med.voll.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/med/voll/api/medicos/Especialidade.java
@@ -0,0 +1,10 @@
+package med.voll.api.medicos;
+
+public enum Especialidade {
+
+    ORTOPEDIA,
+    CARDIOLOGIA,
+    GINECOLOGIA,
+    DERMATOLOGIA;
+
+}
