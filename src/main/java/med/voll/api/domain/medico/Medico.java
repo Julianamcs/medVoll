1990539b05d46795ef25fd36bbdd483c806b78d5
Juliana Maciel <julyanamcs@gmail.com>
2023-07-02 11:20:48 -0300

    lidando com erros
    lidando com erros, melhorando os erros que são expostos na tela

--- src/main/java/med/voll/api/domain/medico/Medico.java
+++ src/main/java/med/voll/api/domain/medico/Medico.java
@@ -1,13 +1,11 @@
-package med.voll.api.medico;
+package med.voll.api.domain.medico;
 
 import jakarta.persistence.*;
-import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import med.voll.api.endereco.Endereco;
-
+import med.voll.api.domain.endereco.Endereco;
 
 
 @Table(name = "medicos")
