2c4f4ecea072f9dcb7897e79a83c8f68a55373d5
Juliana Maciel <julyanamcs@gmail.com>
2023-07-05 23:08:31 -0300

    Novas Funcionalidades
    Consulta, DadosDetalhamentoConsulta, DadosAgendamentoConuslta, RepositoryConsulta e ControllerConsulta

---
+++ src/main/java/med/voll/api/domain/consulta/ConsultaReoisitory.java
@@ -0,0 +1,6 @@
+package med.voll.api.domain.consulta;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConsultaReoisitory extends JpaRepository<Consulta, Long> {
+}
